f0ce50e5111d105639a9686960f53db9ba85cbee
Jonas Souza <Jonassouza871@hotmail.com>
2025-11-12 11:08:40 -0300

    Add counter system to prevent infinite loops in while True context

--- ui/core/basic_blocks/generator_basic.js
+++ ui/core/basic_blocks/generator_basic.js
@@ -1,6 +1,6 @@
 // ==========================================
 // Basic Blocks - Code Generators
-// Python code generators for control and logic blocks
+// Python code generators for repetition blocks
 // ==========================================
 
 console.log('[BitdogLab] Loading basic code generators...');
@@ -14,164 +14,99 @@ Blockly.Python['controls_repeat_simple'] = function(block) {
   var times = block.getFieldValue('TIMES');
   var statements = Blockly.Python.statementToCode(block, 'DO');
 
-  var code = 'for _i in range(' + times + '):\n' +
-             (statements || '  pass\n');
-  return code;
-};
+  // Remove initial indentation (Blockly adds 2 spaces)
+  if (statements) {
+    statements = statements.replace(/^  /gm, '');
+  }
 
-// While loop generator
-Blockly.Python['controls_while_simple'] = function(block) {
-  var condition = Blockly.Python.valueToCode(block, 'CONDITION',
-                  Blockly.Python.ORDER_NONE) || 'False';
-  var statements = Blockly.Python.statementToCode(block, 'DO');
+  // Remove sound block markers
+  statements = statements.replace(/# SOUND_BLOCK_START|# SOUND_BLOCK_END/g, '');
 
-  var code = 'while ' + condition + ':\n' +
-             (statements || '  pass\n');
-  return code;
-};
+  // Add counter initialization to setup (outside loop)
+  Blockly.Python.definitions_['setup_contador_repeticao'] = '_contador_repeticao = 0';
 
-// For loop generator
-Blockly.Python['controls_for_simple'] = function(block) {
-  var variable = Blockly.Python.nameDB_.getName(
-      block.getFieldValue('VAR'), Blockly.Names.NameType.VARIABLE);
-  var from = Blockly.Python.valueToCode(block, 'FROM',
-             Blockly.Python.ORDER_NONE) || '0';
-  var to = Blockly.Python.valueToCode(block, 'TO',
-           Blockly.Python.ORDER_NONE) || '10';
-  var statements = Blockly.Python.statementToCode(block, 'DO');
+  // Use counter to run N times
+  var code = 'if _contador_repeticao < ' + times + ':\n';
+  code += '  _contador_repeticao += 1\n';
 
-  var code = 'for ' + variable + ' in range(' + from + ', ' + to + ' + 1):\n' +
-             (statements || '  pass\n');
+  if (statements && statements.trim()) {
+    // Add indentation line by line
+    var lines = statements.split('\n');
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i].trim() !== '') {
+        code += '  ' + lines[i] + '\n';
+      }
+    }
+  }
+
   return code;
 };
 
 // Repeat forever generator
+/*
 Blockly.Python['controls_repeat_forever'] = function(block) {
   var statements = Blockly.Python.statementToCode(block, 'DO');
 
-  var code = 'while True:\n' +
-             (statements || '  pass\n');
+  // Remove initial indentation (Blockly adds 2 spaces)
+  if (statements) {
+    statements = statements.replace(/^  /gm, '');
+  }
+
+  // Remove sound block markers
+  statements = statements.replace(/# SOUND_BLOCK_START|# SOUND_BLOCK_END/g, '');
+
+  var code = 'while True:\n';
+
+  if (statements && statements.trim()) {
+    // Add indentation line by line
+    var lines = statements.split('\n');
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i].trim() !== '') {
+        code += '  ' + lines[i] + '\n';
+      }
+    }
+  } else {
+    code += '  pass\n';
+  }
+
   return code;
 };
+*/
 
 // Repeat until generator
+/*
 Blockly.Python['controls_repeat_until'] = function(block) {
   var condition = Blockly.Python.valueToCode(block, 'CONDITION',
                   Blockly.Python.ORDER_NONE) || 'False';
   var statements = Blockly.Python.statementToCode(block, 'DO');
 
-  var code = 'while not (' + condition + '):\n' +
-             (statements || '  pass\n');
+  // Remove initial indentation (Blockly adds 2 spaces)
+  if (statements) {
+    statements = statements.replace(/^  /gm, '');
+  }
+
+  // Remove sound block markers
+  statements = statements.replace(/# SOUND_BLOCK_START|# SOUND_BLOCK_END/g, '');
+
+  // Use LOOP markers to prevent automatic while True wrapper
+  var code = '# LOOP_BLOCK_START\n';
+  code += 'while not (' + condition + '):\n';
+
+  if (statements && statements.trim()) {
+    // Add indentation line by line
+    var lines = statements.split('\n');
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i].trim() !== '') {
+        code += '  ' + lines[i] + '\n';
+      }
+    }
+  } else {
+    code += '  pass\n';
+  }
+
+  code += '# LOOP_BLOCK_END\n';
   return code;
 };
-
-// For loop with step generator
-Blockly.Python['controls_for_step'] = function(block) {
-  var variable = Blockly.Python.nameDB_.getName(
-      block.getFieldValue('VAR'), Blockly.Names.NameType.VARIABLE);
-  var from = Blockly.Python.valueToCode(block, 'FROM',
-             Blockly.Python.ORDER_NONE) || '0';
-  var to = Blockly.Python.valueToCode(block, 'TO',
-           Blockly.Python.ORDER_NONE) || '10';
-  var step = Blockly.Python.valueToCode(block, 'STEP',
-             Blockly.Python.ORDER_NONE) || '1';
-  var statements = Blockly.Python.statementToCode(block, 'DO');
-
-  var code = 'for ' + variable + ' in range(' + from + ', ' + to + ' + 1, ' + step + '):\n' +
-             (statements || '  pass\n');
-  return code;
-};
-
-// ==========================================
-// LOGIC GENERATORS
-// ==========================================
-
-// If block generator
-Blockly.Python['controls_if_simple'] = function(block) {
-  var condition = Blockly.Python.valueToCode(block, 'CONDITION',
-                  Blockly.Python.ORDER_NONE) || 'False';
-  var statements = Blockly.Python.statementToCode(block, 'DO');
-
-  var code = 'if ' + condition + ':\n' +
-             (statements || '  pass\n');
-  return code;
-};
-
-// If-else block generator
-Blockly.Python['controls_if_else_simple'] = function(block) {
-  var condition = Blockly.Python.valueToCode(block, 'CONDITION',
-                  Blockly.Python.ORDER_NONE) || 'False';
-  var doStatements = Blockly.Python.statementToCode(block, 'DO');
-  var elseStatements = Blockly.Python.statementToCode(block, 'ELSE');
-
-  var code = 'if ' + condition + ':\n' +
-             (doStatements || '  pass\n') +
-             'else:\n' +
-             (elseStatements || '  pass\n');
-  return code;
-};
-
-// ==========================================
-// COMPARISON GENERATORS
-// ==========================================
-
-// Comparison operators generator
-Blockly.Python['logic_compare_simple'] = function(block) {
-  var operators = {
-    'EQ': '==',
-    'NEQ': '!=',
-    'LT': '<',
-    'LTE': '<=',
-    'GT': '>',
-    'GTE': '>='
-  };
-  var operator = operators[block.getFieldValue('OP')];
-  var order = Blockly.Python.ORDER_RELATIONAL;
-  var a = Blockly.Python.valueToCode(block, 'A', order) || '0';
-  var b = Blockly.Python.valueToCode(block, 'B', order) || '0';
-
-  var code = a + ' ' + operator + ' ' + b;
-  return [code, order];
-};
-
-// ==========================================
-// LOGICAL OPERATORS GENERATORS
-// ==========================================
-
-// AND / OR operator generator
-Blockly.Python['logic_operation_simple'] = function(block) {
-  var operator = (block.getFieldValue('OP') == 'AND') ? 'and' : 'or';
-  var order = (operator == 'and') ? Blockly.Python.ORDER_LOGICAL_AND :
-                                     Blockly.Python.ORDER_LOGICAL_OR;
-  var a = Blockly.Python.valueToCode(block, 'A', order) || 'False';
-  var b = Blockly.Python.valueToCode(block, 'B', order) || 'False';
-
-  var code = a + ' ' + operator + ' ' + b;
-  return [code, order];
-};
-
-// NOT operator generator
-Blockly.Python['logic_not_simple'] = function(block) {
-  var order = Blockly.Python.ORDER_LOGICAL_NOT;
-  var value = Blockly.Python.valueToCode(block, 'BOOL', order) || 'False';
-
-  var code = 'not ' + value;
-  return [code, order];
-};
-
-// ==========================================
-// DELAY GENERATOR
-// ==========================================
-
-// Wait/Delay generator
-Blockly.Python['time_delay_simple'] = function(block) {
-  var seconds = block.getFieldValue('SECONDS');
-
-  // Add import for time module
-  Blockly.Python.definitions_['import_time'] = 'import time';
-
-  var code = 'time.sleep(' + seconds + ')\n';
-  return code;
-};
+*/
 
 console.log('[BitdogLab] Basic code generators loaded successfully!');
