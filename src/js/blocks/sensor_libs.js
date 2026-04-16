c081016a2dda3e7a2541d22c39f121d5ce2a3c1a
Jonas Souza <jonassouza871@hotmail.com>
2026-04-15 21:03:39 -0300

    fix sh1107 landscape

--- src/js/blocks/sensor_libs.js
+++ src/js/blocks/sensor_libs.js
@@ -49,95 +49,137 @@ var SensorLibs = {
   // SH1107 - Display OLED I2C
   // Fonte base: firmware/PyLibs/sh1107.py
   // =============================================
-  SH1107:
-    'from micropython import const\n' +
-    'import time\n' +
-    'import framebuf\n' +
-    '\n' +
-    '_HIGH_COLUMN_ADDRESS = const(0x10)\n' +
-    '_MEM_ADDRESSING_MODE = const(0x20)\n' +
-    '_SET_CONTRAST = const(0x8100)\n' +
-    '_SET_SEGMENT_REMAP = const(0xA0)\n' +
-    '_SET_MULTIPLEX_RATIO = const(0xA800)\n' +
-    '_SET_NORMAL_INVERSE = const(0xA6)\n' +
-    '_SET_DISPLAY_OFFSET = const(0xD300)\n' +
-    '_SET_DC_DC_CONVERTER_SF = const(0xAD81)\n' +
-    '_SET_DISPLAY_OFF = const(0xAE)\n' +
-    '_SET_DISPLAY_ON = const(0xAF)\n' +
-    '_SET_PAGE_ADDRESS = const(0xB0)\n' +
-    '_SET_SCAN_DIRECTION = const(0xC0)\n' +
-    '_SET_DISP_CLK_DIV = const(0xD550)\n' +
-    '_SET_DIS_PRECHARGE = const(0xD922)\n' +
-    '_SET_VCOM_DSEL_LEVEL = const(0xDB35)\n' +
-    '\n' +
-    'class SH1107(framebuf.FrameBuffer):\n' +
-    '  def __init__(self, width, height, external_vcc=False, delay_ms=50):\n' +
-    '    self.width = width\n' +
-    '    self.height = height\n' +
-    '    self.external_vcc = external_vcc\n' +
-    '    self.delay_ms = delay_ms\n' +
-    '    self.pages = height // 8\n' +
-    '    self.row_width = width // 8\n' +
-    '    self.buffer = bytearray(self.pages * self.width)\n' +
-    '    super().__init__(self.buffer, width, height, framebuf.MONO_HMSB)\n' +
-    '    self.init_display()\n' +
-    '\n' +
-    '  def init_display(self):\n' +
-    '    _mux = 0x7F if self.height == 128 else 0x3F\n' +
-    '    _offset = 0x00 if self.height == 128 else 0x60\n' +
-    '    self.reset()\n' +
-    '    self.write_command(bytes([_SET_DISPLAY_OFF]))\n' +
-    '    self.fill(0)\n' +
-    '    self.write_command((_SET_MULTIPLEX_RATIO | _mux).to_bytes(2, "big"))\n' +
-    '    self.write_command((_MEM_ADDRESSING_MODE | 0x01).to_bytes(1, "big"))\n' +
-    '    self.write_command(bytes([_SET_PAGE_ADDRESS]))\n' +
-    '    self.write_command(_SET_DC_DC_CONVERTER_SF.to_bytes(2, "big"))\n' +
-    '    self.write_command(_SET_DISP_CLK_DIV.to_bytes(2, "big"))\n' +
-    '    self.write_command(_SET_VCOM_DSEL_LEVEL.to_bytes(2, "big"))\n' +
-    '    self.write_command(_SET_DIS_PRECHARGE.to_bytes(2, "big"))\n' +
-    '    self.write_command((_SET_CONTRAST | 0x80).to_bytes(2, "big"))\n' +
-    '    self.write_command(bytes([_SET_NORMAL_INVERSE]))\n' +
-    '    self.write_command((_SET_DISPLAY_OFFSET | _offset).to_bytes(2, "big"))\n' +
-    '    self.write_command(bytes([_SET_SEGMENT_REMAP | 0x01]))\n' +
-    '    self.write_command(bytes([_SET_SCAN_DIRECTION]))\n' +
-    '    self.write_command(bytes([_SET_DISPLAY_ON]))\n' +
-    '    time.sleep_ms(self.delay_ms)\n' +
-    '\n' +
-    '  def show(self):\n' +
-    '    _mv = memoryview(self.buffer)\n' +
-    '    _row_cmd = bytearray(2)\n' +
-    '    for _row in range(self.height):\n' +
-    '      _row_cmd[0] = _row & 0x0F\n' +
-    '      _row_cmd[1] = _HIGH_COLUMN_ADDRESS | (_row >> 4)\n' +
-    '      self.write_command(_row_cmd)\n' +
-    '      _start = _row * self.row_width\n' +
-    '      self.write_data(_mv[_start:_start + self.row_width])\n' +
-    '\n' +
-    '  def reset(self):\n' +
-    '    pass\n' +
-    '\n' +
-    'class SH1107_I2C(SH1107):\n' +
-    '  def __init__(self, width, height, i2c, res=None, address=0x3C, rotate=0, external_vcc=False, delay_ms=50):\n' +
-    '    self.i2c = i2c\n' +
-    '    self.address = address\n' +
-    '    self.res = res\n' +
-    '    if res is not None:\n' +
-    '      res.init(res.OUT, value=1)\n' +
-    '    super().__init__(width, height, external_vcc, delay_ms)\n' +
-    '\n' +
-    '  def write_command(self, command_list):\n' +
-    '    self.i2c.writeto(self.address, b"\\x00" + command_list)\n' +
-    '\n' +
-    '  def write_data(self, buf):\n' +
-    '    self.i2c.writevto(self.address, (b"\\x40", buf))\n' +
-    '\n' +
-    '  def reset(self):\n' +
-    '    if self.res is not None:\n' +
-    '      self.res(1)\n' +
-    '      time.sleep_ms(1)\n' +
-    '      self.res(0)\n' +
-    '      time.sleep_ms(20)\n' +
-    '      self.res(1)\n' +
-    '      time.sleep_ms(20)\n'
+  SH1107: `from micropython import const
+import time
+import framebuf
+
+_LOW_COLUMN_ADDRESS = const(0x00)
+_HIGH_COLUMN_ADDRESS = const(0x10)
+_MEM_ADDRESSING_MODE = const(0x20)
+_SET_CONTRAST = const(0x8100)
+_SET_SEGMENT_REMAP = const(0xA0)
+_SET_MULTIPLEX_RATIO = const(0xA800)
+_SET_NORMAL_INVERSE = const(0xA6)
+_SET_DISPLAY_OFFSET = const(0xD300)
+_SET_DC_DC_CONVERTER_SF = const(0xAD81)
+_SET_DISPLAY_OFF = const(0xAE)
+_SET_DISPLAY_ON = const(0xAF)
+_SET_PAGE_ADDRESS = const(0xB0)
+_SET_SCAN_DIRECTION = const(0xC0)
+_SET_DISP_CLK_DIV = const(0xD550)
+_SET_DIS_PRECHARGE = const(0xD922)
+_SET_VCOM_DSEL_LEVEL = const(0xDB35)
+
+class SH1107(framebuf.FrameBuffer):
+  def __init__(self, width, height, external_vcc=False, delay_ms=50, rotate=0):
+    self.width = width
+    self.height = height
+    self.external_vcc = external_vcc
+    self.delay_ms = delay_ms
+    self.rotate = rotate
+    self.rotate90 = rotate == 90 or rotate == 270
+    self.flip_flag = False
+    self.inverse = False
+    if self.rotate90:
+      self.width, self.height = self.height, self.width
+      _mode = framebuf.MONO_VLSB
+    else:
+      _mode = framebuf.MONO_HMSB
+    self.pages = self.height // 8
+    self.buffer = bytearray(self.pages * self.width)
+    self.buffer_mv = memoryview(self.buffer)
+    super().__init__(self.buffer, self.width, self.height, _mode)
+    self.init_display()
+
+  def init_display(self):
+    _mux = 0x7F if self.height == 128 else 0x3F
+    self.reset()
+    self.write_command(bytes([_SET_DISPLAY_OFF]))
+    self.fill(0)
+    self.write_command((_SET_MULTIPLEX_RATIO | _mux).to_bytes(2, "big"))
+    self.write_command((_MEM_ADDRESSING_MODE | (0x00 if self.rotate90 else 0x01)).to_bytes(1, "big"))
+    self.write_command(bytes([_SET_PAGE_ADDRESS]))
+    self.write_command(_SET_DC_DC_CONVERTER_SF.to_bytes(2, "big"))
+    self.write_command(_SET_DISP_CLK_DIV.to_bytes(2, "big"))
+    self.write_command(_SET_VCOM_DSEL_LEVEL.to_bytes(2, "big"))
+    self.write_command(_SET_DIS_PRECHARGE.to_bytes(2, "big"))
+    self.contrast(0x80)
+    self.invert(0)
+    self.flip(False, update=False)
+    self.write_command(bytes([_SET_DISPLAY_ON]))
+    time.sleep_ms(self.delay_ms)
+
+  def contrast(self, contrast):
+    self.write_command((_SET_CONTRAST | (contrast & 0xFF)).to_bytes(2, "big"))
+
+  def invert(self, invert=0):
+    self.write_command((_SET_NORMAL_INVERSE | (invert & 1)).to_bytes(1, "big"))
+    self.inverse = bool(invert)
+
+  def flip(self, flag=None, update=False):
+    if flag is None:
+      flag = not self.flip_flag
+    if self.height == 128 and self.width == 128:
+      _row_offset = 0x00
+    elif self.rotate90:
+      _row_offset = 0x60
+    else:
+      _row_offset = 0x20 if (self.rotate == 180) ^ flag else 0x60
+    _remap = 0x00 if (self.rotate in (90, 180)) ^ flag else 0x01
+    _direction = 0x08 if (self.rotate in (180, 270)) ^ flag else 0x00
+    self.write_command((_SET_DISPLAY_OFFSET | _row_offset).to_bytes(2, "big"))
+    self.write_command(bytes([_SET_SEGMENT_REMAP | _remap]))
+    self.write_command(bytes([_SET_SCAN_DIRECTION | _direction]))
+    self.flip_flag = flag
+    if update:
+      self.show()
+
+  def show(self):
+    if self.rotate90:
+      _cmd = bytearray(3)
+      _cmd[1] = _LOW_COLUMN_ADDRESS
+      _cmd[2] = _HIGH_COLUMN_ADDRESS
+      for _page in range(self.pages):
+        _cmd[0] = _SET_PAGE_ADDRESS | _page
+        self.write_command(_cmd)
+        _start = self.width * _page
+        self.write_data(self.buffer_mv[_start:_start + self.width])
+    else:
+      _row_bytes = self.width // 8
+      _cmd = bytearray(2)
+      for _row in range(self.height):
+        _cmd[0] = _row & 0x0F
+        _cmd[1] = _HIGH_COLUMN_ADDRESS | (_row >> 4)
+        self.write_command(_cmd)
+        _start = _row * _row_bytes
+        self.write_data(self.buffer_mv[_start:_start + _row_bytes])
+
+  def reset(self):
+    pass
+
+class SH1107_I2C(SH1107):
+  def __init__(self, width, height, i2c, res=None, address=0x3C, rotate=0, external_vcc=False, delay_ms=50):
+    self.i2c = i2c
+    self.address = address
+    self.res = res
+    if res is not None:
+      res.init(res.OUT, value=1)
+    super().__init__(width, height, external_vcc, delay_ms, rotate)
+
+  def write_command(self, command_list):
+    self.i2c.writeto(self.address, b"\\x00" + command_list)
+
+  def write_data(self, buf):
+    self.i2c.writevto(self.address, (b"\\x40", buf))
+
+  def reset(self):
+    if self.res is not None:
+      self.res(1)
+      time.sleep_ms(1)
+      self.res(0)
+      time.sleep_ms(20)
+      self.res(1)
+      time.sleep_ms(20)
+`
 
 };
