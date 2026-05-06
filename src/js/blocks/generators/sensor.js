1854b5565c773dd9c670bc815f1dc2becacf3d14
Jonas Souza <jonassouza871@hotmail.com>
2026-05-05 22:18:06 -0300

    Update _plot_grafico to handle LARGE display with adjusted buffer size and positioning

--- src/js/blocks/generators/sensor.js
+++ src/js/blocks/generators/sensor.js
@@ -72,7 +72,7 @@ function _setupEstufaMeasurementDisplay(displayType) {
   }
 }
 
-function _setupEstufaGraficos() {
+function _setupEstufaGraficos(displayType) {
   var pins = BitdogLabConfig.PINS;
   var sensor = BitdogLabConfig.SENSOR;
   var display = BitdogLabConfig.DISPLAY;
@@ -80,12 +80,22 @@ function _setupEstufaGraficos() {
   Blockly.Python.definitions_['import_pin'] = 'from machine import Pin';
   Blockly.Python.definitions_['import_i2c'] = 'from machine import I2C';
   Blockly.Python.definitions_['import_time'] = 'import time';
-  Blockly.Python.definitions_['import_ssd1306'] = 'from ssd1306 import SSD1306_I2C';
-  Blockly.Python.definitions_['lib_aht20'] = SensorLibs.AHT20;
 
-  Blockly.Python.definitions_['setup_display'] =
-    'i2c = I2C(' + display.I2C_BUS + ', scl=Pin(' + pins.I2C_SCL + '), sda=Pin(' + pins.I2C_SDA + '), freq=' + display.I2C_FREQ + ')\n' +
-    'oled = SSD1306_I2C(' + display.WIDTH + ', ' + display.HEIGHT + ', i2c)';
+  if (displayType === 'LARGE') {
+    Blockly.Python.definitions_['lib_sh1107'] = SensorLibs.SH1107;
+    Blockly.Python.definitions_['setup_display'] =
+      'i2c = I2C(' + display.I2C_BUS + ', scl=Pin(' + pins.I2C_SCL + '), sda=Pin(' + pins.I2C_SDA + '), freq=' + display.I2C_FREQ + ')\n' +
+      '_sh1107_scan = i2c.scan()\n' +
+      '_sh1107_addr = 0x3C if 0x3C in _sh1107_scan else (0x3D if 0x3D in _sh1107_scan else 0x3C)\n' +
+      'oled = SH1107_I2C(128, 128, i2c, address=_sh1107_addr, rotate=90)';
+  } else {
+    Blockly.Python.definitions_['import_ssd1306'] = 'from ssd1306 import SSD1306_I2C';
+    Blockly.Python.definitions_['setup_display'] =
+      'i2c = I2C(' + display.I2C_BUS + ', scl=Pin(' + pins.I2C_SCL + '), sda=Pin(' + pins.I2C_SDA + '), freq=' + display.I2C_FREQ + ')\n' +
+      'oled = SSD1306_I2C(' + display.WIDTH + ', ' + display.HEIGHT + ', i2c)';
+  }
+
+  Blockly.Python.definitions_['lib_aht20'] = SensorLibs.AHT20;
 
   var i2c0Sda = pins.I2C0_SDA !== undefined ? pins.I2C0_SDA : pins.I2C_SDA;
   var i2c0Scl = pins.I2C0_SCL !== undefined ? pins.I2C0_SCL : pins.I2C_SCL;
@@ -116,22 +126,76 @@ function _setupEstufaGraficos() {
     'def _dt(x,y,t):\n' +
     '  for c in str(t):_dc(x,y,c);x+=4\n';
 
+  Blockly.Python.definitions_['oled_compat'] =
+    '# OLED compatibility helpers\n' +
+    'def _oled_hline(x, y, w, c):\n' +
+    '  for _x in range(x, x + max(0, w)):\n' +
+    '    oled.pixel(_x, y, c)\n' +
+    'def _oled_vline(x, y, h, c):\n' +
+    '  for _y in range(y, y + max(0, h)):\n' +
+    '    oled.pixel(x, _y, c)\n' +
+    'def _oled_fill_rect(x, y, w, h, c):\n' +
+    '  for _yy in range(y, y + max(0, h)):\n' +
+    '    for _xx in range(x, x + max(0, w)):\n' +
+    '      oled.pixel(_xx, _yy, c)\n' +
+    'def _oled_rect(x, y, w, h, c):\n' +
+    '  if w <= 0 or h <= 0:\n' +
+    '    return\n' +
+    '  _oled_hline(x, y, w, c)\n' +
+    '  _oled_hline(x, y + h - 1, w, c)\n' +
+    '  _oled_vline(x, y, h, c)\n' +
+    '  _oled_vline(x + w - 1, y, h, c)\n' +
+    'def _oled_line(x0, y0, x1, y1, c):\n' +
+    '  dx = abs(x1 - x0)\n' +
+    '  sx = 1 if x0 < x1 else -1\n' +
+    '  dy = -abs(y1 - y0)\n' +
+    '  sy = 1 if y0 < y1 else -1\n' +
+    '  err = dx + dy\n' +
+    '  while True:\n' +
+    '    oled.pixel(x0, y0, c)\n' +
+    '    if x0 == x1 and y0 == y1:\n' +
+    '      break\n' +
+    '    e2 = err * 2\n' +
+    '    if e2 >= dy:\n' +
+    '      err += dy\n' +
+    '      x0 += sx\n' +
+    '    if e2 <= dx:\n' +
+    '      err += dx\n' +
+    '      y0 += sy\n' +
+    'if not hasattr(oled, "hline"):\n' +
+    '  oled.hline = _oled_hline\n' +
+    '  oled.vline = _oled_vline\n' +
+    '  oled.fill_rect = _oled_fill_rect\n' +
+    '  oled.rect = _oled_rect\n' +
+    '  oled.line = _oled_line\n';
+
   Blockly.Python.definitions_['func_plot_grafico'] =
     '_plot_buffers = {}\n' +
-    'def _plot_grafico(buf_id, valor, pos, titulo):\n' +
+    'def _plot_grafico(buf_id, valor, pos, titulo, display_type="SMALL"):\n' +
     '  try:\n' +
     '    val = float(valor)\n' +
     '    if buf_id not in _plot_buffers:\n' +
     '      _plot_buffers[buf_id] = []\n' +
     '    buf = _plot_buffers[buf_id]\n' +
     '    buf.append(val)\n' +
-    '    if len(buf) > 60: buf.pop(0)\n' +
-    '    if pos == 0:\n' +
-    '      y_tit, y_ini, y_fim = 0, 6, 63\n' +
-    '    elif pos == 1:\n' +
-    '      y_tit, y_ini, y_fim = 0, 6, 31\n' +
+    '    max_buf_size = 60\n' +
+    '    actual_height = getattr(oled, "height", 64)\n' +
+    '    if display_type == "LARGE" and actual_height >= 128:\n' +
+    '      max_buf_size = 100\n' +
+    '      if pos == 0:\n' +
+    '        y_tit, y_ini, y_fim = 0, 8, 127\n' +
+    '      elif pos == 1:\n' +
+    '        y_tit, y_ini, y_fim = 0, 8, 62\n' +
+    '      else:\n' +
+    '        y_tit, y_ini, y_fim = 64, 72, 127\n' +
     '    else:\n' +
-    '      y_tit, y_ini, y_fim = 32, 38, 63\n' +
+    '      if pos == 0:\n' +
+    '        y_tit, y_ini, y_fim = 0, 6, 63\n' +
+    '      elif pos == 1:\n' +
+    '        y_tit, y_ini, y_fim = 0, 6, 31\n' +
+    '      else:\n' +
+    '        y_tit, y_ini, y_fim = 32, 38, 63\n' +
+    '    if len(buf) > max_buf_size: buf.pop(0)\n' +
     '    alt = y_fim - y_ini\n' +
     '    oled.fill_rect(0, y_tit, 128, 5, 0)\n' +
     '    _dt(0, y_tit, titulo)\n' +
@@ -306,7 +370,6 @@ Blockly.Python["estufa_toggle_sensor2"] = function(block) {
 };
 
 Blockly.Python["estufa_temp_sensor1"] = function(_block) {
-  _setupEstufaGraficos();
   Blockly.Python.definitions_['func_ler_estufa_temp1'] =
     'def _ler_estufa_temp1():\n' +
     '  d = _aht_esq.get_data() if _aht_esq.is_ready else (None, None)\n' +
@@ -315,7 +378,6 @@ Blockly.Python["estufa_temp_sensor1"] = function(_block) {
 };
 
 Blockly.Python["estufa_umid_sensor1"] = function(_block) {
-  _setupEstufaGraficos();
   Blockly.Python.definitions_['func_ler_estufa_umid1'] =
     'def _ler_estufa_umid1():\n' +
     '  d = _aht_esq.get_data() if _aht_esq.is_ready else (None, None)\n' +
@@ -324,7 +386,6 @@ Blockly.Python["estufa_umid_sensor1"] = function(_block) {
 };
 
 Blockly.Python["estufa_temp_sensor2"] = function(_block) {
-  _setupEstufaGraficos();
   Blockly.Python.definitions_['func_ler_estufa_temp2'] =
     'def _ler_estufa_temp2():\n' +
     '  d = _aht_dir.get_data() if _aht_dir.is_ready else (None, None)\n' +
@@ -333,7 +394,6 @@ Blockly.Python["estufa_temp_sensor2"] = function(_block) {
 };
 
 Blockly.Python["estufa_umid_sensor2"] = function(_block) {
-  _setupEstufaGraficos();
   Blockly.Python.definitions_['func_ler_estufa_umid2'] =
     'def _ler_estufa_umid2():\n' +
     '  d = _aht_dir.get_data() if _aht_dir.is_ready else (None, None)\n' +
@@ -342,18 +402,18 @@ Blockly.Python["estufa_umid_sensor2"] = function(_block) {
 };
 
 Blockly.Python["estufa_plotar"] = function(block) {
-  _setupEstufaGraficos();
+  var displayType = block.getFieldValue('DISPLAY_TYPE') || 'SMALL';
+  _setupEstufaGraficos(displayType);
   var valor = Blockly.Python.valueToCode(block, 'VALOR', Blockly.Python.ORDER_ATOMIC) || '0';
   var rotulo = block.getFieldValue('ROTULO');
   var posicao = block.getFieldValue('POSICAO');
-  var displayType = block.getFieldValue('DISPLAY_TYPE');
   var bufId = 'plot_' + rotulo + '_' + posicao;
   var varName = '_v_' + rotulo + '_' + posicao;
 
   var titulo = "'" + rotulo + ":' + str(round(" + varName + ", 1))";
   var code = varName + ' = (' + valor + ')\n' +
     "_plot_grafico('" + bufId + "', " + varName + ", " + posicao + ", " + titulo + ", '" + displayType + "')\n";
-  return code;
+  return code
 };
 
 Blockly.Python["verificar_conexao_sensor"] = function(_block) {
