14349d7f24ab7a5ef57cf3e9969403195c96f3f5
Jonas Souza <Jonassouza871@hotmail.com>
2026-08-22 21:48:20 -0300

    feat: add distance sensor category icon

--- src/js/bootstrap/page.js
+++ src/js/bootstrap/page.js
@@ -10,7 +10,7 @@
       }
 
       var request = new XMLHttpRequest();
-      request.open('GET', '../js/config/toolbox.xml?ver=20260817category2', true);
+      request.open('GET', '../js/config/toolbox.xml?ver=20260822distance1', true);
       request.onreadystatechange = function() {
         if (request.readyState !== 4 || request.status !== 200) {
           return;
